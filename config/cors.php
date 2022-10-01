aafa8b01efb53dc2d44c84199f6af3dce2900157
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2022-10-01 13:21:29 -0600

    updated cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // 'allowed_origins' => ['*'],
+
+    'allowed_origins' =>  explode(',', env('ALLOWED_ORIGINS', '')),
 
     'allowed_origins_patterns' => [],
 
