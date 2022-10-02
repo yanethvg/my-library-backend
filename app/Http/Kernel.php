a1c0949f4bac0f100a431d2b009bbd826cf6a471
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2022-10-02 16:29:41 -0600

    updated kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,6 @@ class Kernel extends HttpKernel
         //cors
         \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
